732db698ada7ed88a722b58eb48dac2a13301f87
Dinara Urazova <dinara.urazova88@gmail.com>
2024-08-30 11:48:18 +0500

    disply task content, task edit form

--- hello.py
+++ hello.py
@@ -1,18 +1,33 @@
 from flask import Flask, redirect, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
-tasks = [
+chores = [
+    {"id": 1, "name": "Сходить в магазин", "description": "Сходить в 'Байрам'"},
     {
-        "name": "Сходить в магазин"
-    }
+        "id": 2,
+        "name": "Съездить на дачу",
+        "description": "Взять фонарь, купить корм кошкам, не забыть ноутбук",
+    },
+    {"id": 3, "name": "Постирать одежду", "description": "Не позднее пятницы 13-го"},
 ]
+
 
 @app.route("/")
 def root():
     return redirect("/tasks")
 
-@app.route('/tasks')
+
+@app.route("/tasks")
 def get_tasks():
-    return render_template('tasks.html', tasks=tasks)
-    
+    return render_template("tasks.html", tasks=chores)
+
+
+@app.route("/tasks/<int:id>")
+def show_task(id: int):
+    return render_template("task.html", task=chores[id - 1])
+
+
+@app.route("/tasks/<int:id>/edit")
+def edit_task_form(id: int):
+    return render_template("edit.html", task=chores[id - 1])
