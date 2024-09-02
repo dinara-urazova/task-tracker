d27f4f41f0920e265732f53af9beafc58a3a686a
Dinara Urazova <dinara.urazova88@gmail.com>
2024-09-03 04:28:29 +0500

    update existing task with new name and description

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
@@ -13,21 +13,37 @@ chores = [
 ]
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def root():
     return redirect("/tasks")
 
 
-@app.route("/tasks")
+@app.route("/tasks", methods=["GET"])
 def get_tasks():
     return render_template("tasks.html", tasks=chores)
 
 
-@app.route("/tasks/<int:id>")
+@app.route("/tasks/<int:id>", methods=["GET"])
 def show_task(id: int):
     return render_template("task.html", task=chores[id - 1])
 
 
-@app.route("/tasks/<int:id>/edit")
+@app.route("/tasks/<int:id>/edit", methods=["GET"])
 def edit_task_form(id: int):
     return render_template("edit.html", task=chores[id - 1])
+
+
+@app.route("/tasks/<int:id>/update", methods=["POST"])
+def update_task(id: int):
+    print(f"id = {id}")
+
+    new_name = request.form["task_name"]
+    new_desription = request.form["task_description"]
+
+    chores[id - 1] = {
+        "id": id,
+        "name": new_name,
+        "description": new_desription,
+    }
+
+    return redirect(f"/tasks/{id}")
