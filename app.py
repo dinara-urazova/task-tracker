a6fa122f4d89de6b589cf02f6d1ffecc0f1859b3
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2024-11-12 19:16:42 +0300

    lesson 79: functional tests using pytest mock app task storage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import abort, Flask, redirect, render_template, request
+from flask import abort, Flask, redirect, render_template, request, current_app
 from task import Task
 from task_storage_sqlite import TaskStorageSQLite
 from task_storage_json import TaskStorageJson
@@ -7,24 +7,34 @@ from datetime import datetime, timezone
 
 
 app = Flask(__name__)
-task_storage = TaskStoragePostgreSQL()
+app.config['task_storage'] = TaskStoragePostgreSQL()
 # task_storage = TaskStorageSQLite()
 # task_storage = TaskStorageJson()
 
 
 @app.route("/", methods=["GET"])
 def root():
+    """
+    Что должен проверть авто тест?
+
+    1. Делает HTTP-запрос GET /
+    2. Проверят, что сервер ответил
+    2.1 status = 302 (Redirect)
+    2.2 Response Header "Location" = "/tasks"
+    """
     return redirect("/tasks")
 
 
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
+    task_storage = current_app.config['task_storage']
     chores = task_storage.read_all()
     return render_template("tasks.html", tasks=chores)
 
 
 @app.route("/tasks/<string:id>", methods=["GET"])
 def show_task(id: str):
+    task_storage = current_app.config['task_storage']
     task_to_show = task_storage.read_by_id(id)
     if task_to_show == None:
         return abort(404, f"Task with id={id} not found")
@@ -45,12 +55,14 @@ def create_task():
         created_at,
         updated_at,
     )
+    task_storage = current_app.config['task_storage']
     new_task_id = task_storage.create(new_task)
     return redirect(f"/tasks/{new_task_id}")
 
 
 @app.route("/tasks/<string:id>/edit", methods=["GET"])
 def show_edit_task_form(id: str):
+    task_storage = current_app.config['task_storage']
     task_to_edit = task_storage.read_by_id(id)
     # task_to_edit мб None
     return render_template("edit.html", task=task_to_edit, task_id=id)
@@ -81,12 +93,14 @@ def update_task(id: str):
         None,
         updated_at,
     )
+    task_storage = current_app.config['task_storage']
     task_storage.update(id, updated_task)
     return redirect(f"/tasks/{id}")
 
 
 @app.route("/tasks/<string:id>/delete", methods=["GET"])
 def delete_task(id: str):
+    task_storage = current_app.config['task_storage']
     # to do  - прочитать из БД если есть task если нет abort, если есть delete
     task_storage.delete(id)
     return redirect("/tasks")
