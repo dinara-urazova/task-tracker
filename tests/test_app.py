2db06f5d1bf23044de03c706a6d27372a6001c94
Dinara Urazova <dinara.urazova88@gmail.com>
2024-11-26 19:29:07 +0500

    autotests added, to be finished as some tests don't work

--- tests/test_app.py
+++ tests/test_app.py
@@ -81,4 +81,97 @@ def test_show_task_not_found():
     client = app.test_client()
     response = client.get("/tasks/1")
     assert response.status_code == 404
-    assert minify(response.get_data(as_text=True)) == '<!doctype html><html lang=en><title>404 Not Found</title><h1>Not Found</h1><p>Task with id = 1 not found</p>'
+    assert (
+        minify(response.get_data(as_text=True))
+        == "<!doctype html><html lang=en><title>404 Not Found</title><h1>Not Found</h1><p>Task with id = 1 not found</p>"
+    )
+
+
+def test_show_task_found():
+    app.config["task_storage"] = TaskStorageMock(
+        {
+            "read_by_id": lambda id: {
+                "name": "Продукты",
+                "description": "Купить хлеб и молоко",
+            },
+        }
+    )
+    client = app.test_client()
+    response = client.get("/tasks/1")
+    assert response.status_code == 200
+    assert (
+        minify(response.get_data(as_text=True))
+        == '<a href="/tasks">Вернуться на главную</a><h1>Продукты</h1><h2>Описание</h2><p>Купить хлеб и молоко</p><p><em>Дата и время создания:</em></p><p><em>Дата и время последнего изменения:</em></p><a href="/tasks/1/edit">Редактировать</a>'
+    )
+
+
+def test_show_new_task_form():
+    client = app.test_client()
+    response = client.get("/tasks/new")
+    assert response.status_code == 200
+    assert (
+        (response.get_data(as_text=True))
+        == '<a href="/tasks">Назад</a><br><br><form action="/tasks/create" method="post"><label for="task_name">Название:</label><input type="text" id="task_name" name="task_name" required minlength="3" maxlength="100"><br><br><label for="task_description">Описание:</label><textarea id="task_description" name="task_description" rows="10" cols="30" required minlength="3"maxlength="2000"></textarea><br><br><input type="submit" value="Сохранить"></form>'
+    )
+
+
+def test_update_task_not_found():
+    app.config["task_storage"] = TaskStorageMock({"read_by_id": lambda id: None})
+    client = app.test_client()
+    response = client.post("/tasks/1/update")
+    assert response.status_code == 404
+    assert (
+        minify(response.get_data(as_text=True))
+        == "<!doctype html><html lang=en><title>404 Not Found</title><h1>Not Found</h1><p>Task with id = 1 not found</p>"
+    )
+
+
+def test_update_task_found():
+    app.config["task_storage"] = TaskStorageMock(
+        {
+            "read_by_id": lambda id: {
+                "name": "Продукты",
+                "description": "Купить хлеб и молоко",
+            },
+        }
+    )
+    client = app.test_client()
+    response = client.get("/tasks/1/update")
+    assert response.status_code == 200 # всего 2 запроса 302 + 200  
+    assert response.headers.post("Location") == "/tasks/1"
+    # дб какое-то изменение 
+
+def test_delete_task_not_found():
+    app.config["task_storage"] = TaskStorageMock(
+        {
+            "read_by_id": lambda id: None
+        }
+    )
+    client = app.test_client()
+    response = client.get("/tasks/1/delete")
+    assert response.status_code == 404
+    assert (
+        minify(response.get_data(as_text=True))
+        == "<!doctype html><html lang=en><title>404 Not Found</title><h1>Not Found</h1><p>Task with id = 1 not found</p>"
+    )
+    
+    
+    
+    def test_delete_task_found():
+        app.config["task_storage"] = TaskStorageMock(
+        {
+            "read_all": lambda: {
+                1: {
+                    "name": "Отдохнуть",
+                    "description": "Посмотреть фильм",
+                },
+            }
+        }
+    )
+    client = app.test_client()
+    response = client.get("/tasks/1/delete")
+    assert response.status_code == 200 # всего 2 запроса 302 + 200  
+    assert response.headers.post("Location") == "/tasks"
+    # дб какое-то изменение 
+
+ 
