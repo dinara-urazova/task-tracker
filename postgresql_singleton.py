37036d28a201cb27654e2a9ff583f570520b287a
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2024-12-10 18:21:37 +0300

    environment config

--- postgresql_singleton.py
+++ postgresql_singleton.py
@@ -1,4 +1,5 @@
 import pg8000.native
+from config_reader import env_config
 
 
 class PostgreSQLSingleton:
@@ -7,5 +8,11 @@ class PostgreSQLSingleton:
     @classmethod
     def getConnection(cls):
         if cls._connection is None:
-            cls._connection = pg8000.native.Connection("dinaraurazova")
+            cls._connection = pg8000.native.Connection(
+                env_config.postgresql_username,
+                password=env_config.postgresql_password,
+                host=env_config.postgresql_hostname,
+                port=env_config.postgresql_port,
+                database=env_config.postgresql_database,
+            )
         return cls._connection
