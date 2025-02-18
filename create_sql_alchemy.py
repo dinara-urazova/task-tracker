9fcbc8bbbf9ca40dd0713ee9dbf3e9c014003e83
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2025-02-18 18:39:25 +0300

    supress F401 ruff

--- create_sql_alchemy.py
+++ create_sql_alchemy.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 
 from config_reader import env_config
 from entity.base import Base
+# ruff: disable=F401
 from entity.task import Task
+# ruff: disable=F401
 from entity.user import User
+# ruff: disable=F401
 from entity.session import UserSession
 
 connection_str = f"postgresql+pg8000://{env_config.postgresql_username}:{env_config.postgresql_password}@{env_config.postgresql_hostname}:{env_config.postgresql_port}/{env_config.postgresql_database}"
