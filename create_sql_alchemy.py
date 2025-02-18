dd5e903616ed9cdb06752090ea123f8ad2eec619
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2025-02-18 18:42:00 +0300

    fix ruff check

--- create_sql_alchemy.py
+++ create_sql_alchemy.py
@@ -2,12 +2,10 @@ from sqlalchemy import create_engine
 
 from config_reader import env_config
 from entity.base import Base
-# ruff: disable=F401
-from entity.task import Task
-# ruff: disable=F401
-from entity.user import User
-# ruff: disable=F401
-from entity.session import UserSession
+
+from entity.task import Task # noqa: F401
+from entity.user import User # noqa: F401
+from entity.session import UserSession # noqa: F401
 
 connection_str = f"postgresql+pg8000://{env_config.postgresql_username}:{env_config.postgresql_password}@{env_config.postgresql_hostname}:{env_config.postgresql_port}/{env_config.postgresql_database}"
 engine = create_engine(connection_str, echo=True)
